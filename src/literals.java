966d54e9f861dd4c0287c83fba056c041052800f
emmanuel saleem <emmanuelsaleem098765@gmail.com>
2025-10-30 17:51:18 +0500

    first commit

---
+++ src/literals.java
@@ -0,0 +1,23 @@
+public class literals {
+    public static void main (String[] args){
+        System.out.println("new file");
+        byte age = 23;
+        int age2 = 24;
+        short age3 =4;
+        long age4 =555555555555555555L;
+        char name = 'A';
+        float f1 = 5.4f;
+        double dl = 4.444;
+        boolean a = true;
+        String myName = "Emmanuel";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(age4);
+        System.out.println(name);
+        System.out.println(f1);
+        System.out.println(dl);
+        System.out.println(a);
+        System.out.println(myName);
+    }
+}
